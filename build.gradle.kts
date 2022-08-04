f4ecce0e55fc9c854b48065d699e717febe668e3
Tobias Gies <tobias@tobiasgies.de>
2022-08-04 19:00:49 +0200

    Add initial project structure
    Generated with Spring Initializr using the following URL:
    
    https://start.spring.io/#!type=gradle-project&language=kotlin&platformVersion=2.7.2&packaging=jar&jvmVersion=17&groupId=de.tobiasgies&artifactId=ootrtournament&name=ootrtournament&description=OOTR%20Tournament%20Engine&packageName=de.tobiasgies.ootrtournament&dependencies=thymeleaf,webflux,oauth2-client,data-r2dbc,prometheus,security,devtools,configuration-processor,actuator,cloud-starter-sleuth,mariadb

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.2"
+	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+
+	id("com.gorylenko.gradle-git-properties") version "2.4.1"
+
+	kotlin("jvm") version "1.7.10"
+	kotlin("plugin.spring") version "1.7.10"
+}
+
+group = "de.tobiasgies"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2021.0.3"
+
+dependencies {
+	// Spring base application
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
+
+	runtimeOnly("org.mariadb:r2dbc-mariadb")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.springframework.security:spring-security-test")
+
+	// Kotlin adapters and integrations
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	// Observability
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+	implementation("io.sentry:sentry-spring-boot-starter:6.3.1")
+	implementation("io.sentry:sentry-logback:6.3.1")
+
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // External services
+	implementation("com.discord4j:discord4j-core:3.2.2")
+	implementation("com.github.twitch4j:twitch4j:1.11.0")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/de/tobiasgies/ootrtournament/OotrtournamentApplication.kt
@@ -0,0 +1,11 @@
+package de.tobiasgies.ootrtournament
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OotrtournamentApplication
+
+fun main(args: Array<String>) {
+	runApplication<OotrtournamentApplication>(*args)
+}
