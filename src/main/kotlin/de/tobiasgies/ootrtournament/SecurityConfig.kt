1e045a291a9846525473b1971d99a1fc858bb87b
Tobias Gies <tobias@tobiasgies.de>
2022-08-16 00:49:32 +0200

    Add initial INSECURE security config

---
+++ src/main/kotlin/de/tobiasgies/ootrtournament/SecurityConfig.kt
@@ -0,0 +1,39 @@
+package de.tobiasgies.ootrtournament
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.invoke
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity) = http {
+        authorizeExchange {
+            authorize("/", permitAll)
+            authorize("/my/**", authenticated)
+            authorize(anyExchange, denyAll)
+        }
+        formLogin {  }
+    }
+
+    @Bean
+    fun dummyUers(): ReactiveUserDetailsService {
+        val users = User.withDefaultPasswordEncoder()
+        val user = users
+            .username("user")
+            .password("user")
+            .roles("USER")
+            .build()
+        val admin = users
+            .username("admin")
+            .password("admin")
+            .roles("USER", "ADMIN")
+            .build()
+        return MapReactiveUserDetailsService(user, admin)
+    }
+}
