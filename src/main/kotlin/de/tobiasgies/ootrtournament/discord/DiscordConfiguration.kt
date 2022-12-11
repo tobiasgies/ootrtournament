d309e7da55d559cbccfe1d79eb810438a127942e
Tobias Gies <tobias@tobiasgies.de>
2022-12-11 11:37:05 +0100

    Create GatewayDiscordClient bean

--- src/main/kotlin/de/tobiasgies/ootrtournament/discord/DiscordConfiguration.kt
+++ src/main/kotlin/de/tobiasgies/ootrtournament/discord/DiscordConfiguration.kt
@@ -2,7 +2,14 @@ package de.tobiasgies.ootrtournament.discord
 
 import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
+import discord4j.core.event.domain.lifecycle.ConnectEvent
+import discord4j.core.event.domain.lifecycle.DisconnectEvent
+import discord4j.core.event.domain.lifecycle.GatewayLifecycleEvent
 import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.event.domain.lifecycle.ReconnectEvent
+import discord4j.core.event.domain.lifecycle.ReconnectFailEvent
+import discord4j.core.event.domain.lifecycle.ReconnectStartEvent
+import discord4j.core.event.domain.lifecycle.SessionInvalidatedEvent
 import mu.KLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,14 +21,21 @@ class DiscordConfiguration {
     @Bean
     fun discordClient(token: String) = DiscordClient.create(token)
 
-    // TODO find the right mono method so this returns a GatewayDiscordClient, not a Void
-    fun gatewayDiscordClient(discordClient: DiscordClient) = discordClient.login().map { client ->
-        client.on(ReadyEvent::class.java) {
-            Mono.fromRunnable<Void> {
-                logger.info { "Connected to Discord as ${it.self.username}#${it.self.discriminator}." }
-            }
-        }
-    }.block()
+    @Bean
+    fun gatewayDiscordClient(discordClient: DiscordClient) =
+        discordClient.login().doOnNext { client ->
+            client.on(ReadyEvent::class.java) {
+                Mono.fromRunnable<Void> {
+                    logger.info { "Connection to Discord has become rady. " +
+                            "Logged in as ${it.self.username}#${it.self.discriminator}." }
+                }
+            }.subscribe()
+            client.on(GatewayLifecycleEvent::class.java) {
+                Mono.fromRunnable<Void> {
+                    logger.debug { "Gateway lifecycle event received: $it - shard information: ${it.shardInfo}" }
+                }
+            }.subscribe()
+        }.block()!!
 
     companion object : KLogging()
 }
