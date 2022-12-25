47dc8517fad52696273a7146e15d468a9eb26710
Tobias Gies <tobias@tobiasgies.de>
2022-12-25 12:21:10 +0100

    add initial message with buttons

--- src/main/kotlin/de/tobiasgies/ootrtournament/discord/DiscordTournamentSignupFlow.kt
+++ src/main/kotlin/de/tobiasgies/ootrtournament/discord/DiscordTournamentSignupFlow.kt
@@ -7,41 +7,26 @@ import discord4j.core.`object`.component.SelectMenu
 import discord4j.core.`object`.component.TextInput
 import discord4j.core.`object`.entity.channel.MessageChannel
 import discord4j.core.spec.MessageCreateMono
+import discord4j.discordjson.json.MessageCreateRequest
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class DiscordTournamentSignupFlow {
-    fun buildSignupMessage(tournament: Tournament, messageChannel: MessageChannel) {
-        MessageCreateMono.of(messageChannel).withContent(
+    fun buildSignupMessage(tournament: Tournament): MessageCreateRequest {
+        return MessageCreateRequest.builder().content(
             "Thanks for signing up for **${tournament.name}**! " +
                     "Please let us know a few more pieces of information and then you're good to go!"
-        ).withComponents(
-            ActionRow.of(
-                TextInput.small(
-                    "racetime_profile",
-                    "Your racetime.gg profile URL",
-                    "https://racetime.gg/user/AR4nd0mB1tOfT3xt"
-                )
-            ),
-            ActionRow.of(
-                TextInput.small(
-                    "pronouns",
-                    "Your preferred pronouns",
-                    "they/them"
-                )
-            ),
-            ActionRow.of(
-                SelectMenu.of("country", ISO_COUNTRIES.map {
-                    SelectMenu.Option.of(it.value, it.key)
-                })
-            ),
+        ).addComponent(
             ActionRow.of(
                 Button.primary("rules_consent", "I agree to the rules and Fair Play Agreement"),
                 Button.secondary("rules_reject", "I do not agree")
-            ),
+            ).data
+        ).addComponent(
             ActionRow.of(
                 Button.success("submit", "Submit")
-            )
-        )
+            ).data
+        ).build()
     }
 
     companion object {
--- src/main/kotlin/de/tobiasgies/ootrtournament/web/DiscordSandboxController.kt
+++ src/main/kotlin/de/tobiasgies/ootrtournament/web/DiscordSandboxController.kt
@@ -1,5 +1,7 @@
 package de.tobiasgies.ootrtournament.web
 
+import de.tobiasgies.ootrtournament.discord.DiscordTournamentSignupFlow
+import de.tobiasgies.ootrtournament.entities.Tournament
 import discord4j.common.util.Snowflake
 import discord4j.core.GatewayDiscordClient
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,13 +11,28 @@ import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/discord")
-class DiscordSandboxController(private val gatewayDiscordClient: GatewayDiscordClient) {
+class DiscordSandboxController(
+    private val gatewayDiscordClient: GatewayDiscordClient,
+    private val signupFlow: DiscordTournamentSignupFlow
+    ) {
     @PostMapping("/helloworld")
-    fun testmessage(): Mono<String> {
+    fun helloworld(): Mono<String> {
         return gatewayDiscordClient.getChannelById(Snowflake.of("1051485104096157739"))
             .flatMap {
                 it.restChannel.createMessage("Hello world!")
             }.map { 
+                "Succesfully created message with ID ${it.id()}"
+            }
+    }
+
+    @PostMapping("/signupmessage")
+    fun signupmessage(): Mono<String> {
+        return gatewayDiscordClient.getChannelById(Snowflake.of("1051485104096157739"))
+            .flatMap {
+                it.restChannel.createMessage(
+                    signupFlow.buildSignupMessage(Tournament("Test Tournament"))
+                )
+            }.map {
                 "Succesfully created message with ID ${it.id()}"
             }
     }
